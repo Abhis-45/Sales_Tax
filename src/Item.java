0cf7c53d91d7143af8f063f449adeeaff5fbbe00
Abhishek Kumar <abhisingh12598@gmail.com>
2024-09-24 10:27:36 +0530

    main

---
+++ src/Item.java
@@ -0,0 +1,29 @@
+public class Item {
+    private String name;
+    private double price;
+    private boolean isImported;
+    private boolean isTaxExempt;
+
+    public Item(String name, double price, boolean isImported, boolean isTaxExempt) {
+        this.name = name;
+        this.price = price;
+        this.isImported = isImported;
+        this.isTaxExempt = isTaxExempt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isImported() {
+        return isImported;
+    }
+
+    public boolean isTaxExempt() {
+        return isTaxExempt;
+    }
+}
